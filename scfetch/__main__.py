59857f93f3892bce4c7832a69e7193434dd2835f
sctech-tr <gamerselimiko@gmail.com>
2024-11-17 14:46:51 +0300

    1.8.0

--- scfetch/__main__.py
+++ scfetch/__main__.py
@@ -9,31 +9,102 @@ import os
 import platform
 
 # these are the imports for the fetch
-from .os_info import get_os_info
-from .python_version import get_python_version
-from .cpu import get_cpu_info
-from .user import get_username
-from .machine_name import get_machine_name
-from .arch import get_arch
-from .private_ip import get_private_ip
-from .public_ip import get_public_ip
-from .kernel_ver import get_kernel_ver
-from .mac import get_mac_address
-from .ram import get_ram
-from .pc import get_pc_model
-from .gpu import get_gpu_info
-from .uptime import get_uptime
-from .shell import get_shell
-from .screen_resolution import get_screen_resolution
-from .disk_usage import get_disk_usage
-from .battery import get_battery_status
-from .locale_info import get_locale
-from .de_wm import get_wm
+from scfetch.os_info import get_os_info
+from scfetch.python_version import get_python_version
+from scfetch.cpu import get_cpu_info
+from scfetch.user import get_username
+from scfetch.machine_name import get_machine_name
+from scfetch.arch import get_arch
+from scfetch.private_ip import get_private_ip
+from scfetch.public_ip import get_public_ip
+from scfetch.kernel_ver import get_kernel_ver
+from scfetch.mac import get_mac_address
+from scfetch.ram import get_ram
+from scfetch.pc import get_pc_model
+from scfetch.gpu import get_gpu_info
+from scfetch.uptime import get_uptime
+from scfetch.shell import get_shell
+from scfetch.screen_resolution import get_screen_resolution
+from scfetch.disk_usage import get_disk_usage
+from scfetch.battery import get_battery_status
+from scfetch.locale_info import get_locale
+from scfetch.de_wm import get_wm
 
 # --- END RANDOM IMPORTS NO ONE READS ---
 
+def define_ascii():
+    lines = []
+    if platform.system() == "Windows":
+        lines = [
+            f"{Color.BLUE}                    ....,,:;+ccllll  {Color.OFF}",
+            f"{Color.BLUE}     ...,,+:;  clllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE},cclllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}                                     {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}llllllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}`'ccllllllllll  lllllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}      `' ''*::  :ccllllllllllllllll  {Color.OFF}",
+            f"{Color.BLUE}                      ````'' *::cll  {Color.OFF}"
+        ]
+    elif platform.system() == "Linux":
+        lines = [
+            f"{Color.BLACK}            a8888b.         {Color.OFF}",
+            f"{Color.BLACK}          d888888b.         {Color.OFF}",
+            f"{Color.BLACK}          8P'YP'Y88         {Color.OFF}",
+            f"{Color.BLACK}          8|{Color.OFF}{Color.WHITE}O{Color.OFF}{Color.BLACK}||{Color.OFF}{Color.WHITE}O{Color.OFF}{Color.BLACK}|88         {Color.OFF}",
+            f"{Color.BLACK}          8{Color.OFF}{Color.YELLOW}'    .{Color.OFF}{Color.BLACK}88         {Color.OFF}",
+            f"{Color.BLACK}          8{Color.OFF}{Color.YELLOW}`._.'{Color.OFF}{Color.BLACK} Y8.        {Color.OFF}",
+            f"{Color.BLACK}        d/      `8b.        {Color.OFF}",
+            f"{Color.BLACK}      .dP   .     Y8b.      {Color.OFF}",
+            f"{Color.BLACK}      d8:'   '   `::88b.    {Color.OFF}",
+            f"{Color.BLACK}     d8'           `Y88b    {Color.OFF}",
+            f"{Color.BLACK}    :8P     '       :888    {Color.OFF}",
+            f"{Color.BLACK}     8a.    :      _a88P    {Color.OFF}",
+            f"{Color.BLACK}   {Color.OFF}{Color.YELLOW}._/'Y{Color.OFF}{Color.BLACK}aa_     :.{Color.OFF}{Color.YELLOW} -----    {Color.OFF}",
+            rf"{Color.BLACK}   {Color.OFF}{Color.YELLOW}\    YP'      `{Color.OFF}{Color.YELLOW}|     `.  {Color.OFF}",
+            rf"{Color.BLACK}   {Color.OFF}{Color.YELLOW}/     \.{Color.OFF}{Color.BLACK}_____.d{Color.OFF}{Color.YELLOW}|    {Color.OFF}{Color.YELLOW} .'  {Color.OFF}",
+            f"{Color.BLACK}   {Color.OFF}{Color.YELLOW}`--..__){Color.OFF}{Color.BLACK}888888P{Color.OFF}{Color.YELLOW}`._.'     {Color.OFF}",
+            f"{Color.BLACK}                            {Color.OFF}",
+            f"{Color.BLACK}                            {Color.OFF}"
+        ]
+    elif platform.system() == "Darwin":
+        lines = [
+            f"{Color.RED}              ___           {Color.OFF}",
+            f"{Color.RED}           ⢀⣴⣿⣿⡿           {Color.OFF}",
+            f"{Color.RED}          ⢀⣾⣿⣿⠟⠁           {Color.OFF}",
+            f"{Color.RED}  ⢀⣠⣤⣤⣤⣀⣀⠈⠋⠉⣁⣠⣤⣤⣤⣀⡀      {Color.OFF}",
+            f"{Color.RED} ⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦.   {Color.OFF}",
+            f"{Color.RED}⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀  {Color.OFF}",
+            f"{Color.RED}⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀  {Color.OFF}",
+            f"{Color.RED}⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀  {Color.OFF}",
+            f"{Color.RED}⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀  {Color.OFF}",
+            f"{Color.RED}⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀  {Color.OFF}",
+            f"{Color.RED} ⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁  {Color.OFF}",
+            f"{Color.RED}  ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀  {Color.OFF}",
+            f"{Color.RED}    ⠈⠙⢿⣿⣿⣿⠿⠟⠛⠻⠿⣿⣿⣿⡿⠋⠀⠀⠀ {Color.OFF}",
+            f"{Color.RED}                           {Color.OFF}",
+            f"{Color.RED}                           {Color.OFF}",
+            f"{Color.RED}                           {Color.OFF}",
+            f"{Color.RED}                           {Color.OFF}",
+            f"{Color.RED}                           {Color.OFF}"
+        ]
+    return lines
+
 # real fetch
 def main():
+
+    # ascii art
+    lines = define_ascii()
+
     color_raw = "cyan"
     config_paths = {
         "Windows": "%USERPROFILE%/scfetch.json",
@@ -63,29 +134,30 @@ def main():
         "white": Color.WHITE
     }
     color = color_map.get(color_raw, Color.CYAN)
-    print(f"{Effect.BOLD}{color}{get_username()}{Color.OFF}{Effect.OFF}@{Effect.BOLD}{color}{get_machine_name()}{Color.OFF}{Effect.OFF}") # what
-    print(f"{color}{'=' * 50}{Color.OFF}")
+
     # i had to test this a million times to get the space count working
-    print(f"{Effect.BOLD}{color}OS{Color.OFF}{Effect.OFF}:               {get_os_info()}")
-    print(f"{Effect.BOLD}{color}Host{Color.OFF}{Effect.OFF}:             {get_pc_model()}")
-    print(f"{Effect.BOLD}{color}Kernel{Color.OFF}{Effect.OFF}:           {get_kernel_ver()}")
-    print(f"{Effect.BOLD}{color}Uptime{Color.OFF}{Effect.OFF}:           {get_uptime()}")
-    print(f"{Effect.BOLD}{color}Arch{Color.OFF}{Effect.OFF}:             {get_arch()}")
-    print(f"{Effect.BOLD}{color}Shell{Color.OFF}{Effect.OFF}:            {get_shell()}")
-    print(f"{Effect.BOLD}{color}Resolution{Color.OFF}{Effect.OFF}:       {get_screen_resolution()}")
-    print(f"{Effect.BOLD}{color}CPU{Color.OFF}{Effect.OFF}:              {get_cpu_info()}")
-    print(f"{Effect.BOLD}{color}GPU{Color.OFF}{Effect.OFF}:              {get_gpu_info()}")
-    print(f"{Effect.BOLD}{color}RAM{Color.OFF}{Effect.OFF}:              {get_ram()}")
-    print(f"{Effect.BOLD}{color}DE/WM{Color.OFF}{Effect.OFF}:            {get_wm()}")
-    print(f"{Effect.BOLD}{color}Battery{Color.OFF}{Effect.OFF}:          {get_battery_status()}")
-    print(f"{Effect.BOLD}{color}Disk Usage{Color.OFF}{Effect.OFF}:       {get_disk_usage()}")
-    print(f"{Effect.BOLD}{color}Python Version{Color.OFF}{Effect.OFF}:   {get_python_version()}")
-    print(f"{Effect.BOLD}{color}Locale{Color.OFF}{Effect.OFF}:           {get_locale()}")
-    print(f"{Effect.BOLD}{color}Local IP{Color.OFF}{Effect.OFF}:         {get_private_ip()}")
-    print(f"{Effect.BOLD}{color}Public IP{Color.OFF}{Effect.OFF}:        {get_public_ip()}")
-    print(f"{Effect.BOLD}{color}MAC Address{Color.OFF}{Effect.OFF}:      {get_mac_address()}")
-    print(f"{Color.RED}■■■■{Color.OFF}{Color.YELLOW}■■■■{Color.OFF}{Color.GREEN}■■■■{Color.OFF}{Color.CYAN}■■■■{Color.OFF}{Color.BLUE}■■■■{Color.OFF}{Color.MAGENTA}■■■■{Color.OFF}{Color.BLUE}■■■■{Color.OFF}{Color.CYAN}■■■■{Color.OFF}{Color.GREEN}■■■■{Color.OFF}{Color.YELLOW}■■■■{Color.OFF}{Color.RED}■■■■{Color.OFF}")
-    print(f"{Color.RED}■■■■{Color.OFF}{Color.YELLOW}■■■■{Color.OFF}{Color.GREEN}■■■■{Color.OFF}{Color.CYAN}■■■■{Color.OFF}{Color.BLUE}■■■■{Color.OFF}{Color.MAGENTA}■■■■{Color.OFF}{Color.BLUE}■■■■{Color.OFF}{Color.CYAN}■■■■{Color.OFF}{Color.GREEN}■■■■{Color.OFF}{Color.YELLOW}■■■■{Color.OFF}{Color.RED}■■■■{Color.OFF}")
+    print(f"                            {Effect.BOLD}{color}{get_username()}{Color.OFF}{Effect.OFF}@{Effect.BOLD}{color}{get_machine_name()}{Color.OFF}{Effect.OFF}")
+    print(f"                            {color}{'=' * 50}{Color.OFF}")
+    print(f"{lines[0]}{Effect.BOLD}{color}OS{Color.OFF}{Effect.OFF}:               {get_os_info()}")
+    print(f"{lines[1]}{Effect.BOLD}{color}Host{Color.OFF}{Effect.OFF}:             {get_pc_model()}")
+    print(f"{lines[2]}{Effect.BOLD}{color}Kernel{Color.OFF}{Effect.OFF}:           {get_kernel_ver()}")
+    print(f"{lines[3]}{Effect.BOLD}{color}Uptime{Color.OFF}{Effect.OFF}:           {get_uptime()}")
+    print(f"{lines[4]}{Effect.BOLD}{color}Arch{Color.OFF}{Effect.OFF}:             {get_arch()}")
+    print(f"{lines[5]}{Effect.BOLD}{color}Shell{Color.OFF}{Effect.OFF}:            {get_shell()}")
+    print(f"{lines[6]}{Effect.BOLD}{color}Resolution{Color.OFF}{Effect.OFF}:       {get_screen_resolution()}")
+    print(f"{lines[7]}{Effect.BOLD}{color}CPU{Color.OFF}{Effect.OFF}:              {get_cpu_info()}")
+    print(f"{lines[8]}{Effect.BOLD}{color}GPU{Color.OFF}{Effect.OFF}:              {get_gpu_info()}")
+    print(f"{lines[9]}{Effect.BOLD}{color}RAM{Color.OFF}{Effect.OFF}:              {get_ram()}")
+    print(f"{lines[10]}{Effect.BOLD}{color}DE/WM{Color.OFF}{Effect.OFF}:            {get_wm()}")
+    print(f"{lines[11]}{Effect.BOLD}{color}Battery{Color.OFF}{Effect.OFF}:          {get_battery_status()}")
+    print(f"{lines[12]}{Effect.BOLD}{color}Disk Usage{Color.OFF}{Effect.OFF}:       {get_disk_usage()}")
+    print(f"{lines[13]}{Effect.BOLD}{color}Python Version{Color.OFF}{Effect.OFF}:   {get_python_version()}")
+    print(f"{lines[14]}{Effect.BOLD}{color}Locale{Color.OFF}{Effect.OFF}:           {get_locale()}")
+    print(f"{lines[15]}{Effect.BOLD}{color}Local IP{Color.OFF}{Effect.OFF}:         {get_private_ip()}")
+    print(f"{lines[16]}{Effect.BOLD}{color}Public IP{Color.OFF}{Effect.OFF}:        {get_public_ip()}")
+    print(f"{lines[17]}{Effect.BOLD}{color}MAC Address{Color.OFF}{Effect.OFF}:      {get_mac_address()}")
+    print(f"                            {Color.RED}■■■■{Color.OFF}{Color.YELLOW}■■■■{Color.OFF}{Color.GREEN}■■■■{Color.OFF}{Color.CYAN}■■■■{Color.OFF}{Color.BLUE}■■■■{Color.OFF}{Color.MAGENTA}■■■■{Color.OFF}{Color.BLUE}■■■■{Color.OFF}{Color.CYAN}■■■■{Color.OFF}{Color.GREEN}■■■■{Color.OFF}{Color.YELLOW}■■■■{Color.OFF}{Color.RED}■■■■{Color.OFF}")
+    print(f"                            {Color.RED}■■■■{Color.OFF}{Color.YELLOW}■■■■{Color.OFF}{Color.GREEN}■■■■{Color.OFF}{Color.CYAN}■■■■{Color.OFF}{Color.BLUE}■■■■{Color.OFF}{Color.MAGENTA}■■■■{Color.OFF}{Color.BLUE}■■■■{Color.OFF}{Color.CYAN}■■■■{Color.OFF}{Color.GREEN}■■■■{Color.OFF}{Color.YELLOW}■■■■{Color.OFF}{Color.RED}■■■■{Color.OFF}")
 
 if __name__ == "__main__":
     main()
